ab8babb23d461b3b4799ae0c73ed53d1b51c2a6b
Matt Palmer <mpalmer@hezmatt.org>
2020-04-03 17:39:09 +1100

    Add YAML serialization to OpenSSL::X509::Name

--- lib/openssl/yaml_serialization.rb
+++ lib/openssl/yaml_serialization.rb
@@ -1,22 +1,17 @@
 require "openssl"
 
-class OpenSSL::X509::Certificate
-  def encode_with(coder)
-    coder['der'] = self.to_der
-  end
+module OpenSSL::YamlSerialization
+  module DER
+    def encode_with(coder)
+      coder['der'] = self.to_der
+    end
 
-  def init_with(coder)
-    self.__send__(:initialize, coder['der'])
-  end
-end
-
-class OpenSSL::PKey::PKey
-  def encode_with(coder)
-    coder['der'] = self.to_der
-  end
-
-  def init_with(coder)
-    self.__send__(:initialize, coder['der'])
+    def init_with(coder)
+      self.__send__(:initialize, coder['der'])
+    end
   end
 end
 
+OpenSSL::X509::Certificate.prepend(OpenSSL::YamlSerialization::DER)
+OpenSSL::X509::Name.prepend(OpenSSL::YamlSerialization::DER)
+OpenSSL::PKey::PKey.prepend(OpenSSL::YamlSerialization::DER)
